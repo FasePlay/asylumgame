7bdd2925bbe50b099d26969ad557b64a872cbdba
Oleksandr Minzov <playfase228@gmail.com>
2023-08-22 14:28:12 +0200

    Game screen transition

---
+++ Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    private int room;
+    [SerializeField] private Animator screenAnimator;
+
+    [SerializeField] private GameObject leftArrow;
+    [SerializeField] private GameObject rightArrow;
+
+    private void Start()
+    {
+        screenAnimator = GetComponent<Animator>();
+
+        room = 0;
+        screenAnimator.SetInteger("room", room);
+    }
+
+    public void MoveScreen(int direction)
+    {
+        room += direction;
+        screenAnimator.SetInteger("room", room);
+
+        if (room == 0)
+        {
+            leftArrow.SetActive(true);
+            rightArrow.SetActive(true);
+        }
+        if (room == -1) leftArrow.SetActive(false);
+        if (room == 1) rightArrow.SetActive(false);
+        
+        
+    }
+}
